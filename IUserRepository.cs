88e64855964440b24f74a62a478837b6af6120f2
pokorimbogom-afk <pokorimbogom@gmail.com>
2026-03-29 21:16:17 +0300

    Simplified role system - manual DB management

--- IUserRepository.cs
+++ IUserRepository.cs
@@ -4,5 +4,4 @@ public interface IUserRepository
 {
     Task<bool> UserExists(string username);
     Task CreateUser(string username, string passwordHash);
-    Task<bool> UpdateUserRole(string username, string role);
 }
