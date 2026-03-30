42d31898dc29513007276b1fbb8dbc560cf828f6
pokorimbogom-afk <pokorimbogom@gmail.com>
2026-03-30 11:50:44 +0300

    Fixed CORS and GitHub Raw URLs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 
+// Добавляем CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
+
+// Используем CORS
+app.UseCors("AllowAll");
+
+// Добавляем статические файлы для launcher и loader
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "launcher")),
+    RequestPath = "/launcher"
+});
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new Microsoft.Extensions.FileProviders.PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "loader")),
+    RequestPath = "/loader"
+});
+
 app.MapControllers();
 app.Run();
